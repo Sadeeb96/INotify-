febeac16de267a1e90c9ee009bbd74c7ed65655d
[Ahsan Sadeeb] <[sadeeb.mufc@gmail.com]>
2021-08-06 17:16:14 +0600

    Added Callback

--- include/Inotify.h
+++ include/Inotify.h
@@ -5,19 +5,23 @@
 #include "defines.h"
 #include <iostream>
 #include <string>
+#include <functional>
 class Inotify
 {
 private:
+    std::function<void(char*)> _cBack;
     int _fileD;
     int _watchD;
     std::string path;
     char buffer[BUFFER_LEN];
     int _mode=0;
+    
 public:
     template <class... T2>
     Inotify(std:: string path, T2... modes){
         this->path = path;
         setModes(modes...);
+        
     }
     void init(){
         _fileD = inotify_init();
@@ -32,25 +36,34 @@ public:
     ~Inotify(){
 
     }
-private:
+private:    
     void start(){
         int len;
         while (true)
         {
             len = read(_fileD,buffer,BUFFER_LEN);
-            std::cout<<"CHANGE"<<std::endl;
+            std::cout<<"Change"<<" "<<len<<std::endl;
+            
+            int i=0;
+            while (i<len)
+            {
+                inotify_event* event = (inotify_event*)&buffer[i];
+                bool isDir = event->mask & IN_ISDIR;
+                _cBack(event->name);
+                i+= EVENT_SIZE + event->len;
+            }
             
         }
         
     }
     template <class T>
     void setModes(T var){
-        std::cout<<var<<std::endl;
-        _mode = _mode | getMode(var);
+        //_mode = _mode | getMode(var);
+        _cBack = var;
+        
     }
     template <class T1,class... T2>
     void setModes(T1 arg1 ,T2... args){
-        std::cout<<arg1<<std::endl;
         _mode = _mode | getMode(arg1);
         setModes(args...);
     }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include "include/Inotify.h"
 using namespace std;
 int main(){
-    Inotify i = Inotify("/home/ahsan/ascipc/","modify","create");
+    Inotify i = Inotify("/home/ahsan/ascipc/myfile","modify","open",[](char* path){
+        cout<<"File "<<path<<" Modified \n";
+    });
     i.init();
     i.startAsync();
     
